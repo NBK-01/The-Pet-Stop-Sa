3591c3f655ab83fc9989cb4466fbddc6df5f0093
unknown <nayefkanaan03@gmail.com>
2021-12-02 20:04:22 +0300

    Adding Post model

---
+++ server/models/Post.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const PostSchema = new Schema({
+  postDate: {
+    type: Date,
+    default: Date.now
+  },
+  title: 
+    {
+      type: String,
+      required: true,
+  },
+  description: {
+      type: String,
+      required: true,
+  },
+});
+
+const Post = mongoose.model('post', PostSchema);
+
+module.exports = Post;
