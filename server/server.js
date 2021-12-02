7e52dcc49504b39740d810be27816b364f70a7f1
unknown <nayefkanaan03@gmail.com>
2021-12-02 19:43:49 +0300

    Adding resolvers and typedefs

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-onst express = require('express');
+const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 
@@ -19,8 +19,7 @@ server.applyMiddleware({ app });
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// Serve up static assets
-app.use('/images', express.static(path.join(__dirname, '../client/images')));
+
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
