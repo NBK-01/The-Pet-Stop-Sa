1b75ef2cf3082900008594b3b685d5a1a2ead2f8
unknown <nayefkanaan03@gmail.com>
2021-12-05 01:31:46 +0300

    Initial post resolvers and typedefs

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,24 @@
-// const {Post} = require('../models')
+const { Post } = require('../models');
 
 const resolvers = {
-    Query: {
-      hello: () => 'Hello World!'
-        },
-    }
+  Query: {
+    posts: async () => {
+      return Post.find();
+    },
+
+    post: async (parent, { postId }) => {
+      return Post.findOne({ _id: postId });
+    },
+  },
+
+  Mutation: {
+    addPost: async (parent, { title, description, category }) => {
+      return Post.create({title, description, category});
+    },
+    removePost: async (parent, { postId }) => {
+      return Post.findOneAndDelete({ _id: postId });
+    },
+  },
+};
+
+module.exports = resolvers;
--- server/schemas/typedefs.js
+++ server/schemas/typedefs.js
@@ -3,7 +3,22 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
 
-type Query{
-   hello: String!
+type Post {
+   _id: ID
+   title: String
+   description: String
+   category: String
 }
+
+type Query{
+   Posts: [Post]!
+   post(postId: ID!): Post
+}
+
+
+type Mutation {
+   addPost(title: String!, description: String!, category: String!): Post  
+   removePost(postId: ID!): Post
+}
+
 `
