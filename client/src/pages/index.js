35baf52a96ed6a8a4b302d3f617bce4d844792df
unknown <nayefkanaan03@gmail.com>
2021-12-05 22:11:44 +0300

    Importing components to hoem page

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,3 +1,17 @@
 import React, {useState} from 'react';
 import MobileNav from '../components/mobileNav';
 import NavBar from '../components/Navbar';
+
+
+
+const Home = () => {
+        return (
+            <div>
+                <MobileNav/>
+                <NavBar/> 
+            </div>
+        )
+    }
+    
+    export default Home
+    
