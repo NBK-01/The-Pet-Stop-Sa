c611f91fa0c8f3cd1e4ea92a1dc5ee28fbce918b
unknown <nayefkanaan03@gmail.com>
2021-12-06 00:33:45 +0300

    importing new font family into main app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import Home from '../src/pages/index'
+import GlobalFonts from './fonts/fonts';
 
 function App() {
   return (
     <div>
+      <GlobalFonts/>
       <Home/>
+
     </div>
   );
 }
