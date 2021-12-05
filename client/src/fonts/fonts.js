a44e5ab549329b47969286efd795667e9598f271
unknown <nayefkanaan03@gmail.com>
2021-12-06 00:33:24 +0300

    Renaming fonts and exporting extra font family

--- client/src/fonts/fonts.js
+++ client/src/fonts/fonts.js
@@ -2,6 +2,9 @@ import { createGlobalStyle } from 'styled-components';
 
 import ManropeWoff from './Manrope.woff';
 import ManropeWoff2 from './Manrope.woff2';
+import ManropeBoldWoff from './ManropeB.woff';
+import ManropeBoldWoff2 from './ManropeB.woff2';
+
 
 export default createGlobalStyle`
     @font-face {
@@ -12,18 +15,13 @@ export default createGlobalStyle`
         font-weight: 300;
         font-style: normal;
     }
-`;
-
-import ManropeBoldWoff from './Manrope Bold.woff';
-import ManropeBoldWoff2 from './Manrope Bold.woff2';
-
-export default createGlobalStyle`
     @font-face {
-        font-family: 'Manrope Bold';
+        font-family: 'ManropeB';
         src: local('Manrope Bold'), local('Manrope Bold'),
         url(${ManropeBoldWoff2}) format('woff2'),
         url(${ManropeBoldWoff}) format('woff');
-        font-weight: 300;
+        font-weight: 800;
         font-style: normal;
     }
 `;
+
