96926f634f22150e2c4b82334e89330a7fd8f705
unknown <nayefkanaan03@gmail.com>
2021-12-06 22:40:43 +0300

    Importing Autherror and signtokens

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,6 @@
 const { Post, User } = require('../models');
+const { signToken } = require('../utils/auth')
+const {AuthenticationError} = require('apollo-server-express')
 
 const resolvers = {
   Query: {
@@ -24,28 +26,7 @@ const resolvers = {
   },
 
   Mutation: {
-    addUser: async (parent, { username, email, password }) => {
-      const user = await User.create({ username, email, password });
-      const token = signToken(user);
-      return { token, user };
-    },
-    login: async (parent, { email, password }) => {
-      const user = await User.findOne({ email });
-
-      if (!user) {
-        throw new AuthenticationError('No user found with this email address');
-      }
-
-      const correctPw = await user.isCorrectPassword(password);
-
-      if (!correctPw) {
-        throw new AuthenticationError('Incorrect credentials');
-      }
-
-      const token = signToken(user);
-
-      return { token, user };
-    },
+    
     addPost: async (parent, { title, description, category }) => {
       return Post.create({title, description, category});
     },
