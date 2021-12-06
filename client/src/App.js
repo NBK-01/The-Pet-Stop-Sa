ebfc9436f02b1ecc46db16c54222385cf723bd77
unknown <nayefkanaan03@gmail.com>
2021-12-06 04:05:08 +0300

    Debugging router bug

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './App.css';
 import Home from '../src/pages/index'
 import GlobalFonts from './fonts/fonts';
-
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
+    <Router>
       <GlobalFonts/>
       <Home/>
-    </div>
+    </Router>
   );
 }
 
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,7 +1,8 @@
 import React, {useState} from 'react';
-// import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import NavBar from '../components/Navbar';
 import About from '../components/heroView';
+import MobileNav from '../components/mobileNav'
 import {aboutObjOne} from '../components/heroView/data'
 
 
