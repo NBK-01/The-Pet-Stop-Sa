a46ab9ecdac16a8c8825647c7b7dc891d5d32d7d
unknown <nayefkanaan03@gmail.com>
2021-12-06 06:18:21 +0300

    Adding in hero copy to data fileZ

--- client/src/components/heroView/data.js
+++ client/src/components/heroView/data.js
@@ -3,9 +3,8 @@ export const aboutObjOne = {
     lightBg: false,
     lightText: true,
     lightTextDesc: true,
-    topLine: 'NO CLUE', 
-    headline: 'HEADLINE TINGS',
-    description: 'BLah blah blahblahblahb  blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah',
+    headline: 'Find your next best friend',
+    description: 'a quick, easy, and safe place to adopt or buy pets around the KSA',
     imgStart: false,
     // img: require('../../images/mobile.svg'),
     alt: 'reading book' 
