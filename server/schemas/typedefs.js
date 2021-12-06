6835a41ecf3c7dc4fa270416f2d1c2e3cb8b5462
unknown <nayefkanaan03@gmail.com>
2021-12-06 22:50:18 +0300

    Adding user typedef"

--- server/schemas/typedefs.js
+++ server/schemas/typedefs.js
@@ -3,6 +3,15 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
 
+
+type User {
+   _id: ID
+    username: String
+    email: String
+    password: String
+    posts: [Post]!
+}
+
 type Post {
    _id: ID
    title: String
