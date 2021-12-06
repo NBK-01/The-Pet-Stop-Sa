d98c87e3ea78e9cca8b326102d2c0e6180d85f10
unknown <nayefkanaan03@gmail.com>
2021-12-06 22:39:44 +0300

    Adding user query resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,17 +1,51 @@
-const { Post } = require('../models');
+const { Post, User } = require('../models');
 
 const resolvers = {
   Query: {
-    post: async () => {
-      return Post.find();
+    users: async () => {
+      return User.find().populate('posts');
     },
-
+    user: async (parent, { username }) => {
+      return User.findOne({ username }).populate('posts');
+    },
+    posts: async (parent, { username }) => {
+      const params = username ? { username } : {};
+      return Post.find(params).sort({ createdAt: -1 });
+    },
     post: async (parent, { postId }) => {
       return Post.findOne({ _id: postId });
+    },
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id }).populate('posts');
+      }
+      throw new AuthenticationError('You need to be logged in!');
     },
   },
 
   Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('No user found with this email address');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
     addPost: async (parent, { title, description, category }) => {
       return Post.create({title, description, category});
     },
