5af77cfd78bb6fbfd16588e73b7c8913e02004f7
unknown <nayefkanaan03@gmail.com>
2021-12-07 22:19:12 +0300

    Finalizing post Styles

--- client/src/components/postList/postEl.js
+++ client/src/components/postList/postEl.js
@@ -7,17 +7,24 @@ width: 100%;
 display: flex;
 align-items: center;
 justify-content: center;
+overflow: hidden;
+
 `
 
 export const PostContainer = styled.div`
-max-height: 200px;
-max-width: 900px;
+max-height: 30rem;
+max-width: 60rem;
 padding: 5rem;
 background-color: #fff;
 box-shadow: 0 1.4rem 8rem rgba(0,0,0,0.2);
 display: flex;
 align-items: center;
 border-radius: 10px;
+
+@media screen and (max-width: 768px) {
+    max-height: 10rem;
+    max-width: 20rem
+}
 `
 
 
@@ -25,9 +32,10 @@ border-radius: 10px;
 
 
 export const PostImg = styled.div`
-min-width:25rem;
+min-width:10rem;
 max-width:25rem;
-height: 20rem;
+min-height: 5rem;
+max-height: 20rem;
 transform: translateX(-8rem);
 position: relative;
 `
@@ -56,7 +64,7 @@ border: 5px solid #fb8500;
 export const PostCategory = styled.span` 
 display: block;
 color: rgba(251, 133, 0, 0.5);
-font-size: 15px;
+font-size: 0.8rem;
 font-family: Manrope;
 font-weight: 800;
 margin: .5rem 0;
@@ -65,7 +73,7 @@ margin: .5rem 0;
 export const PostAuthor = styled.span`
 display: block;
 color: rgba(251, 133, 0, 0.5);
-font-size: 12px;
+font-size: 0.8rem;
 font-weight: 800;
 font-family: Manrope;
 margin: .5rem 0;
@@ -73,21 +81,28 @@ margin: .5rem 0;
 
 export const PostTitle = styled.h1`
 font-weight: 800; 
-font-size: 30px;
+font-size: 1.8rem;
 text-decoration: none;
 font-family: ManropeB;
 /* line-height: 82px; */
 color: rgba(251, 133, 0, 1);
 margin: 1.5rem 0 2rem;
+
+
+
 `
 export const PostDescription = styled.h1`
 /* margin-bottom: 3rem; */
-font-size: 23px;
+font-size: 1.3rem;
 font-weight: 800;
 text-decoration: none;
 font-family: Manrope;
 color: rgba(0, 0, 0, 0.6);
-/* margin: 1.5rem 0 2rem; */
+margin: 1.5rem 0 2rem;
+
+@media screen and (max-width:768px){
+    font-size: smaller;
+}
 `
 
 
