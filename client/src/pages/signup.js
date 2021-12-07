6f99757edf9d67eb9fd0c0e7e578918098696670
unknown <nayefkanaan03@gmail.com>
2021-12-07 06:09:43 +0300

    Inti sign up page

--- client/src/pages/signup.js
+++ client/src/pages/signup.js
@@ -1,11 +1,12 @@
 import React, {useState} from 'react';
 import NavBar from '../components/Navbar';
 import About from '../components/heroView';
+import Signup from '../components/SignUp'
 
 const SignUp = () => {
     return(
         <>
-      <NavBar/>
+        <Signup />
         </>
     )
 }
