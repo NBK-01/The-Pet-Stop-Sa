8f49a0aef1ebe395cfe2af612ce19d3e84cd8c8c
unknown <nayefkanaan03@gmail.com>
2021-12-07 09:27:23 +0300

    Styling sign up form components

--- client/src/components/SignUp/signupEl.js
+++ client/src/components/SignUp/signupEl.js
@@ -18,6 +18,11 @@ position: fixed;
 top: 0;
 overflow: hidden;
 right: 0;
+overflow: hidden;
+
+@media screen and (max-width:768px){
+    width: 100%;
+}
 `
 
 export const ColumnTwo = styled.div`
@@ -28,6 +33,10 @@ position: fixed;
 top: 0;
 overflow: hidden;
 left: 0;
+
+@media screen and (max-width:768px){
+    display: none;
+}
 `
 
 // export const ImgWrapper = styled.div`
@@ -75,3 +84,113 @@ filter: blur(70px);
 border-radius: 10px;
 transform: rotate(-170.77deg);
 `
+
+
+export const MainContainer = styled.div`
+display: flex;
+align-items: center;
+flex-direction: column;
+height: 80%;
+width: 80%;
+position: absolute;
+top: 10%; 
+right: 10%;
+box-shadow: 0 0 1rem 0 rgba(0, 0, 0, .2);	
+z-index: 1;
+background: inherit;
+border-radius: 8px;
+
+&:before{
+    content: "";
+	position: absolute;
+	background: inherit;
+	z-index: -1;
+	top: 0;
+	left: 0;
+	right: 0;
+	bottom: 0;
+	box-shadow: inset 0 0 2000px rgba(255, 255, 255, .5);
+	filter: blur(10px);
+	margin: -20px;
+}
+`;
+
+export const SignUpHeading = styled.h1`
+font-weight: 800;
+font-size: 40px;
+line-height: 82px;
+font-family: ManropeB;
+text-transform: lowercase;
+color: black;
+`
+
+
+export const PetStop = styled.span`
+color: #fb8500;
+text-decoration: underline;
+`
+
+
+
+export const FormContainer = styled.form`
+display: flex;
+flex-direction: column;
+justify-content: space-around;
+align-items: center;
+height: 40%;
+width: 100%;
+`
+
+
+
+export const Input = styled.input`
+  background: rgba(255, 255, 255, 0.15);
+  box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.14);
+  border-radius: 5px;
+  width: 50%;
+  height: 2rem;
+  padding: 1rem;
+  border: none;
+  outline: none;
+  font-weight: 800;
+  font-size: 15px;
+  line-height: 75px;
+  font-family: ManropeB;
+  color: black;
+  &:focus {
+    display: inline-block;
+    transform: scale(1.1);
+    border-radius: 7px;
+  }
+  /* &::placeholder {
+    color: #b9abe099;
+    font-weight: 100;
+    font-size: 1rem;
+  } */
+`;
+
+export const BtnWrapper = styled.div`
+margin: 1rem 0 2rem 0;
+width: 100%;
+display: flex;
+align-items: center;
+justify-content: center;
+`
+
+export const SignUpBtn = styled.button`
+  background: #fb8500;
+  text-transform: uppercase;
+  letter-spacing: 0.2rem;
+  width: 15%;
+  height: 3rem;
+  border: none;
+  color: white;
+  font-size: 15px;
+  border-radius: 5px;
+  cursor: pointer;
+  font-family: Manrope;
+  font-weight: 400;
+  line-height: 19px;
+`;
+
+
