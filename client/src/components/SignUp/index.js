6c921f313a9e4a32eeaea88e17e1c588ffca45e8
unknown <nayefkanaan03@gmail.com>
2021-12-07 09:31:14 +0300

    Importing mutations for signup

--- client/src/components/SignUp/index.js
+++ client/src/components/SignUp/index.js
@@ -1,10 +1,51 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { Link } from 'react-router-dom'
+import {useMutation} from '@apollo/client'
+import { ADD_USER } from '../../utils/mutations'
+import Auth from '../../utils/auth'
+
+
+
+
+
 import {ColumnOne, ColumnTwo, Img} from './signupEl'
 import { BlurEl, BlurElTwo } from './signupEl'
 import { MainContainer, SignUpHeading, PetStop, FormContainer, Input, BtnWrapper, SignUpBtn}from './signupEl'
 
 
 const SignUp = () => {
+    const [formState, setFormState] = useState({
+        username: '',
+        email: '',
+        password: '',
+      });
+      const [addUser, { error, data }] = useMutation(ADD_USER);
+    
+      const handleChange = (event) => {
+        const { name, value } = event.target;
+    
+        setFormState({
+          ...formState,
+          [name]: value,
+        });
+      };
+    
+      const handleFormSubmit = async (event) => {
+        event.preventDefault();
+        console.log(formState);
+    
+        try {
+          const { data } = await addUser({
+            variables: { ...formState },
+          });
+    
+          Auth.login(data.addUser.token);
+        } catch (e) {
+          console.error(e);
+        }
+      };
+
+
     return (
            <>
 
