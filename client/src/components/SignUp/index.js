0aa8109a3939c9f7dcf1ac483006afc5666b2039
unknown <nayefkanaan03@gmail.com>
2021-12-07 09:27:04 +0300

    Setting up sign up form

--- client/src/components/SignUp/index.js
+++ client/src/components/SignUp/index.js
@@ -1,12 +1,26 @@
 import React from 'react'
 import {ColumnOne, ColumnTwo, Img} from './signupEl'
 import { BlurEl, BlurElTwo } from './signupEl'
+import { MainContainer, SignUpHeading, PetStop, FormContainer, Input, BtnWrapper, SignUpBtn}from './signupEl'
+
 
 const SignUp = () => {
     return (
            <>
-           <ColumnOne> </ColumnOne>
 
+           <ColumnOne>
+            <MainContainer>
+                <SignUpHeading> sign up to <PetStop> the pet stop </PetStop> </SignUpHeading>
+                <FormContainer>
+                    <Input/>
+                    <Input/>
+                    <Input/>
+                </FormContainer>
+                <BtnWrapper>
+                    <SignUpBtn> Sign-Up </SignUpBtn>
+                </BtnWrapper>
+            </MainContainer>
+           </ColumnOne>
 
            <ColumnTwo> 
                     <BlurEl/>
