c82a574e35b7bcd9309d95066020cb43a13889a6
unknown <nayefkanaan03@gmail.com>
2021-12-07 16:11:56 +0300

    Adding signin page

---
+++ client/src/pages/signin.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Signin from '../components/SignIn'
+
+const SignIn = () => {
+    return(
+        <>
+        <Signin />
+        </>
+    )
+}
+
+
+export default Signin
