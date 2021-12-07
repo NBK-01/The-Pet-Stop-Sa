0f51e12b2d233bef333f9b78843c0c76ae0eff56
unknown <nayefkanaan03@gmail.com>
2021-12-07 16:25:20 +0300

    Adding scripts for auth and form functions

--- client/src/components/SignIn/index.js
+++ client/src/components/SignIn/index.js
@@ -1,11 +1,49 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {ColumnOne, BlurElTwo, BlurEl, Img, ColumnTwo, MainContainer, SignUpHeading, Input, BtnWrapper, FormContainer, PetStop, SignUpBtn} from '../SignUp/signupEl'
 
 import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../../utils/mutations';
 
-const Signin = () => {
+import Auth from "../../utils/auth"
+
+
+
+
+const Signin = (props) => {
+        const [formState, setFormState] = useState({ email: '', password: '' });
+        const [login, { error, data }] = useMutation(LOGIN_USER);
+      
+       
+        const handleChange = (event) => {
+          const { name, value } = event.target;
+      
+          setFormState({
+            ...formState,
+            [name]: value,
+          });
+        };
+      
+      
+        const handleFormSubmit = async (event) => {
+          event.preventDefault();
+          console.log(formState);
+          try {
+            const { data } = await login({
+              variables: { ...formState },
+            });
+      
+            Auth.login(data.login.token);
+          } catch (e) {
+            console.error(e);
+          }
+      
+          
+          setFormState({
+            email: '',
+            password: '',
+          });
+        };      
     return (
         <>
             <ColumnOne> 
@@ -17,15 +55,24 @@ const Signin = () => {
            <ColumnTwo>
             <MainContainer>
                 <SignUpHeading> Welcome back to <PetStop> the pet stop </PetStop> </SignUpHeading>
+                {data ? (
+              <p>
+                Success! You may now head{' '}
+                <Link to="/">back to the homepage.</Link>
+              </p>
+            ) : (
                 <FormContainer>
-                    <Input/>
-                    <Input/>
+                    <Input placeholder="Your email"
+                  name="email"
+                  type="email"
+                  value={formState.email}
+                  onChange={handleChange}/>
                     <Input/>
                     <BtnWrapper>
                     <SignUpBtn> Sign-In </SignUpBtn>
                 </BtnWrapper>
                 </FormContainer>
-
+            )}
             </MainContainer>
                 
            </ColumnTwo>
