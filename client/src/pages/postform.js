7cf9d95d720fc3f1bb9be4dbca8176b673c6d9bd
unknown <nayefkanaan03@gmail.com>
2021-12-07 22:58:25 +0300

    Creating form for post listing page

---
+++ client/src/pages/postform.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Signin from '../components/SignIn'
+
+const PostForm = () => {
+    return(
+        <>
+        <Signin />
+        </>
+    )
+}
+
+
+export default PostForm
